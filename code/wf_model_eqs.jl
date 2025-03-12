12dee764d853d1079075b53b905f2aa6537bdb0d
Marie Gutgesell <marie.gutgesell11@gmail.com>
2025-03-12 15:37:53 -0800

    model with omnivory pref and habitat pref working

--- code/wf_model_eqs.jl
+++ code/wf_model_eqs.jl
@@ -8,6 +8,12 @@ Pkg.add("Plots")
 using Parameters: @with_kw, @unpack ##imports Parameters package that provides convenient macros for working with keyword arugemnts, parameter structs and unpacking variables 
 using DifferentialEquations
 using Plots
+using Symbolics
+using ForwardDiff
+using LinearAlgebra
+using NLsolve
+using DataFrames
+using Interact
 
 ##Re-creating model from McCann et al., 2005, Ecology Letters
 #u = state variables where u[1] = R1, u[2] = R2, u[3] = C1, u[4] = C2, u[5] = P
@@ -19,7 +25,7 @@ using Plots
 function hab_pref(u, p, t)
     C1, C2 = u
     return p.w * C1/(p.w * C1 + (1 - p.w)*C2)  
-    end
+end
 
 ##Scale of foraging 
 function forage_scale(u, p, t)
@@ -35,7 +41,7 @@ end
 # Omnivory preference functions
 function deg_om_i(u, p, t) ##constant preference, where degree of omnivory shifts in response to changing resource/consumer densities in given patch i -  and degree of omnivory used in functional response (can consider later adding in dynamic omnivory)
    R1, C1 = u
-    return p.o * (p.e * p.a * R1 / (p.o * p.e * p.a * R1) + (1 - o)*p.e * p.a * C1)
+    return p.o * (p.e * p.a * R1 / (p.o * p.e * p.a * R1) + (1 - p.o)*p.e * p.a * C1)
 end
 ##know the R/C isnt right... 
 ##hmm how do i do this so the state variables change depending on if i or j patch? - come back to this
@@ -43,7 +49,7 @@ end
 
 function deg_om_j(u, p ,t)
     R2, C2 = u
-    return p.o * (p.e * p.a * R2 / (p.o * p.e * p.a * R2) + (1 - o)*p.e * p.a * C2)
+    return p.o * (p.e * p.a * R2 / (p.o * p.e * p.a * R2) + (1 - p.o)*p.e * p.a * C2)
 end
 
 #Set up paramters 
@@ -56,7 +62,6 @@ end
         ##do we need separate w for each C/R? wR1, wR2, wC1, wC2 ? 
     o = 0.5  ##o = omnivory preference, preference for either consumer or resource, starting with fixed omnivory preference (essentially passive case)
 
-
     ##Model parameters, for now just keeping these parameters the same (could make unique ones based on patch/TL)
     r = 1.0
     K = 3.25
@@ -65,18 +70,15 @@ end
     m = 1.0
     h = 0.5
     ##Habitat preference function 
-    h_pref::Function hab_pref  
+    h_pref::Function = hab_pref  
 
     ##Foraging scale
-    S::Function forage_scale
+    S::Function = forage_scale
     
     ##Omnivory preference function
-    d_om_i::Function deg_om_i ##if d_om = 0, have food chain (no omnivory) in patch 1
-    d_om_j::Function deg_om_j ##degree of omnivory in patch 2 
+    d_om_i::Function = deg_om_i ##if d_om = 0, have food chain (no omnivory) in patch 1
+    d_om_j::Function = deg_om_j ##degree of omnivory in patch 2 
     
-   
-
-
 end
 
 
@@ -151,3 +153,113 @@ return du
 end 
 
 
+# Utilities for doing eigenvalue analysis
+##this is a right-hand-side function that is used to solve ODEs 
+function rhs(u, p, t=0.0)
+    du = similar(u) ##creates a new array du with the same type and size as u but uninitialized
+    model!(du, u, p, t) 
+    return du ##this returns the computed derivative (or result of model) stored in du 
+end
+
+##set initial condition
+u0 = [0.66, 0.66, 0.27, 0.27, 0.15]
+tspan = (0.0, 1000.0)
+
+##set Parameters
+p = ModelPar()
+
+
+##Define the ODE problem
+prob = ODEProblem(rhs, u0, tspan, p)
+sol = solve(prob)
+
+##plot timeseries
+plot(sol, xlabel="Time", ylabel="Population", title="ODE Solution")
+
+##NO FUCKING WAY!!!!!!!! I THINK THIS WORKS !!!!! 
+
+##calculate numerical jacobian
+J_num = ForwardDiff.jacobian(x -> rhs(x, p), u0)
+println("Jacobian Matrix:")
+display(J_num)
+##find the equilibrium for this system of equations 
+eq = nlsolve((du, u) -> model!(du, u, p, 0.0), u0).zero
+
+# Display equilibrium
+println("Equilibrium point (R*, C*): ", eq)
+
+
+
+##trying model that holds dP/dt constant ... and maybe calculates changes in harvest? sum of functional responses.. 
+function model_2!(du, u, p ,t)
+    @unpack r, K, a, e, h, m = p
+   R1, R2, C1, C2, P = u 
+   S1 = p.S(u, p, t) ##function that defines foraging scale and habitat preference (since Q = 0, Si = Wi, so Sj = 1-Si)
+   o1 = p.d_om_i(u, p, t) ##function that defines degree of omnivory in patch 1
+   o2 = p.d_om_j(u, p, t) ##function that defines degree of omnivory in patch 2 
+   
+   ##compute these once for all... essentially same as in functional responses above 
+   num_R1P = S1 * a * o1 * R1
+   num_R2P = (1 - S1) * a * o2 * R2
+   num_C1P = S1 * a * (1 - o1) * C1
+   num_C2P = (1 - S1) * a * (1 - o2) * C2
+   denom_RCP =  1 + ((S1 * a * h * o1 * R1) + ((1-S1)* a * h * o2 * R2)) + ((S1 * a * h * (1-o1) * C1) + ((1-S1) * a * h * (1-o2) * C2))
+   f_RC1 = a * R1 / (1 + a * h * R1)
+   f_RC2 = a * R2 / (1 + a * h * R2)
+   
+   ##ODEs
+   du[1] = r * R1 * (1 - R1 / K) - C1 * f_RC1 - P * num_R1P / denom_RCP
+   du[2] = r * R2 * (1 - R2 / K) - C2 * f_RC2 - P * num_R2P / denom_RCP
+   du[3] = e * C1 * f_RC1 - P * num_C1P / denom_RCP - m * C1
+   du[4] = e * C2 * f_RC2 - P * num_C2P / denom_RCP - m * C2 
+   du[5] = 0
+
+   #du[6] = (num_R1P + num_R2P) / denom_RCP + e * P * (num_C1P + num_C2P) / denom_RCP 
+   return du
+ end 
+   
+   
+   # Utilities for doing eigenvalue analysis
+   ##this is a right-hand-side function that is used to solve ODEs 
+   function rhs_2(u, p, t=0.0)
+       du = similar(u) ##creates a new array du with the same type and size as u but uninitialized
+       model_2!(du, u, p, t) 
+       return du ##this returns the computed derivative (or result of model) stored in du 
+   end
+   
+   ##set initial condition
+   u0 = [1.0, 0.8, 0.45, 0.61, 0.2]
+   tspan = (0.0, 1000.0)
+   
+   ##set Parameters
+   p = ModelPar()
+   
+   
+   ##Define the ODE problem
+   prob_2 = ODEProblem(rhs_2, u0, tspan, p)
+   sol_2 = solve(prob_2)
+   
+   ##plot timeseries
+   plot(sol_2, xlabel="Time", ylabel="Population", title="ODE Solution")
+   
+
+
+     ##so this works, holding P constant. then just need to figure out how to calculate total harvest (just adding 2 functional responses together)
+
+##Next steps:
+##work through checks to make sure the math makes sense
+##write out and think about the math to get to the jacobian and the eigenvalues to interpret stability outcomes
+##sensitivity analysis across parameter spaces ... 
+##bifurcations? 
+##stability metrics -- local and non-local 
+
+##how to add in preference for subsidy
+##calculating total harvest -- essentially as sum of functional responses ... 
+##reading and thinking about parameters and how to choose parameters, also different parameters per TL etc. 
+
+ ##thinking about parameters, and then how to set up experiments to test applied questions 
+ ##introducing abiotic asynchrony into resources 
+ ##making model stochastic 
+
+
+ ##much later next steps: thinking about perturbations.. 
